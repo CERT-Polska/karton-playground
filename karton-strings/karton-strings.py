229562ffa5adeec00a67c6b2a50772aeb2f614ba
Jarosław Jedynak <msm@cert.pl>
2021-01-06 15:29:49 +0100

    Work on the karton tutorial

---
+++ karton-strings/karton-strings.py
@@ -0,0 +1,35 @@
+from karton.core import Karton, Task, Resource
+import subprocess
+
+
+class Strings(Karton):
+    """
+    Runs the `strings` utility on incoming samples
+    """
+
+    identity = "karton.strings"
+    filters = [{"type": "sample", "stage": "recognized"}]
+
+    def process(self, task: Task) -> None:
+        # Get the incoming sample
+        sample_resource = task.get_resource("sample")
+
+        # Log with self.log
+        self.log.info(f"Hi {sample_resource.name}, let me analyse you!")
+
+        # Download the resource to a temporary file
+        with sample_resource.download_temporary_file() as sample_file:
+            # And run `strings` on it
+            strings = subprocess.check_output(["strings", sample_file.name])
+
+        # Send our results for further processing or reporting
+        task = Task(
+            {"type": "sample", "stage": "analyzed"},
+            payload={"parent": sample_resource, "sample": Resource("string", strings)},
+        )
+        self.send_task(task)
+
+
+if __name__ == "__main__":
+    # Here comes the main loop
+    Strings().loop()

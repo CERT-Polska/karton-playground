6ab444d8cf32074ab5fe0c670dfbc400d30bb0bf
Paweł Srokosz <pawel.srokosz@cert.pl>
2021-07-29 18:55:13 +0200

    Bump mwdb version to 2.4.0

--- mwdb-plugin-karton/mwdb_plugin_karton/__init__.py
+++
@@ -1,8 +0,0 @@
-from .plugin import entrypoint, configure
-
-__author__ = "CERT Polska"
-__version__ = "1.0.0"
-__doc__ = "Karton integration plugin for MWDB"
-
-__plugin_entrypoint__ = entrypoint
-__plugin_configure__ = configure
